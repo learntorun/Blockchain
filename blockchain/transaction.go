5417ecb58d7ef0d086e2408f32ebd15f4f32e49d
KSKIM <ADMIN@KSKIMs-MacBook-Pro.local>
2019-01-15 16:47:53 +0900

    tranaction model fix

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -16,19 +16,6 @@ type Transaction struct {
 }
 
 
-type TxInput struct {
-	ID []byte
-	Out int
-	Sig string
-}
-
-
-type TxOutput struct {
-	Value int
-	PubKey string
-}
-
-
 func CoinbaseTx(to, data string) *Transaction {
 	if data == "" {
 		data = fmt.Sprintf("Coins to %s", to)
@@ -96,12 +83,3 @@ func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
 
-
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
-
-
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
-}
